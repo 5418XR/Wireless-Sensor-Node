8fe1a04948f3b7501bbdbfd716ea6fc400371931
5418XR <72371666+5418XR@users.noreply.github.com>
2022-03-17 21:15:13 -0400

    Update WSN_rongx.ino.ino

--- lab03_rongx.ino/WSN_rongx.ino/WSN_rongx.ino.ino
+++ lab03_rongx.ino/WSN_rongx.ino/WSN_rongx.ino.ino
@@ -1,48 +1,44 @@
 
-int HzOutPut =6;     //output pin
-double inputV = A0;  //input pin
-int outputV = A1;    // not yet coding but it will code
+int HzOutPut =6;
+double inputV = A0;
+int outputV = A1;// not yet coding
 
-double RESISTER_VALUE_ONE = 10;   //resister for the input
-double RESISTER_VALUE_TWO = 100;  //resister for the other resister
+double RESISTER_VALUE_ONE = 30;
+double RESISTER_VALUE_TWO = 120;
 
-int DutyCycle = 50;   //defualt duty cycle
+int DutyCycle = 50;
 
-double  setCharge = 9; // the target charging voltage
-double  outDC= 0;      // the start input dutyCycle
-double  DCtemp = 0;    // the start input dutycycle +1
+double  setCharge = 9;
+double  outDC= 0;
+double  DCtemp = 0;
 
-
-//setup the the 62k Hz clock 
 void setup() {
 Serial.begin(9600);
 TCCR0B = TCCR0B &11111000 | B00000001;//62kHz
 pinMode(HzOutPut, outDC);
 
 }
-
 // main part
 void loop() {
   //get the read from A0
   inputV = analogRead(A0);
-  //set the DutyCycle with PID mofdle
+  //
   double  DCtemp = outDC;
-  outDC = DutyCycleCauculation(inputV, setCharge, DCtemp);// using the function 
-  //setal the output votagle
+  outDC = DutyCycleCauculation(inputV, setCharge, DCtemp);
+
   analogWrite(HzOutPut, outDC);
 }
 
-//the output DutyCycal changing Function
 double DutyCycleCauculation(double inputV, double setCharge, double preDC){
-  double realVin = inputV/RESISTER_VALUE_ONE*(RESISTER_VALUE_ONE+RESISTER_VALUE_TWO);
+  double realVin = (inputV*0.0045)*5;
   Serial.print("Vin:");
   Serial.print(realVin);
 
   double out = preDC;
-  if (realVin< setCharge){
+  if (realVin< setCharge+0.5 && out<50){
     out++;
   }
- else if(realVin > setCharge){
+ else if(realVin > setCharge-0.5 && out>1){
   out--; 
  }
   Serial.print("VoutDC:");
